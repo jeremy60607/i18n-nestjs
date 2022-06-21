1cb03399e940cd6a7523e1ed8cea6c81a64cb824
Jeremy Lu <jeremy60607@gmail.com>
2022-06-22 01:58:02 +0800

    feat: test for i18n

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,32 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { I18n, I18nContext, I18nService } from "nestjs-i18n";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly i18nService: I18nService
 
-  @Get()
+  ) {}
+
+
+  @Get('/i18n')
+  async getHellog(@I18n() i18n: I18nContext) {
+    console.log('\x1b[36m%s\x1b[0m', '======================== Debug ========================\n');
+    console.log('\x1b[35m%s\x1b[0m', '   = ', this.i18nService.getSupportedLanguages());
+    console.log('\x1b[35m%s\x1b[0m', '   = ', i18n);
+    console.log('\x1b[36m%s\x1b[0m', '\n=======================================================');
+    return await i18n.t('test.day_interval', {
+      args: { count: 0 },
+    });
+
+
+  }
+
+
+
+@Get()
   getHello(): string {
     return this.appService.getHello();
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from "nestjs-i18n";
+import * as path from "path";
 
 @Module({
-  imports: [],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      // formatter: (template: string, ...args: any[]) => {
+      //   console.log('\x1b[36m%s\x1b[0m', '======================== Debug ========================\n');
+      //   console.log('\x1b[35m%s\x1b[0m', '  template = ', template);
+      //   console.log('\x1b[36m%s\x1b[0m', '\n=======================================================');
+      //   return template;
+      // },
+      resolvers: [
+        // { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
